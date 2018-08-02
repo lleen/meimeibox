1e159313f999d1a3c56d855863171408bc12732f
lleen <1503509248@qq.com>
2018-08-03 00:35:30 +0800

    小标题

--- src/components/pages/Home/Homes.vue
+++ src/components/pages/Home/Homes.vue
@@ -1,17 +1,21 @@
 <template>
   <div class = "Homes">
     <HomeCarousel></HomeCarousel>
+    <HomeSection></HomeSection>
   </div>
 </template>
 <script>
 // home今日精选界面所有东西放在这个文件
 // 引入轮播图
 import HomeCarousel from './HomeCarousel'
+// 小导航栏
+import HomeSection from './HomeSection'
 
 export default {
   name: 'Homes',
   components: {
-    HomeCarousel
+    HomeCarousel,
+    HomeSection
   }
 }
 </script>
